7e0ac306391b4ed533441632b9760b4ccd1d78fc
Montiel Cesar <montiel_cesar@arnet.com.ar>
2021-02-10 16:36:25 -0300

    Primera Version del Sitio Web

---
+++ website/MisViajes/MisViajes/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MisViajes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Acerca del Sitio.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Pagina de Contacto.";
+
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            ViewBag.Message = "Panel de Usuario.";
+
+            return View();
+        }
+
+        public ActionResult Profile()
+        {
+            ViewBag.Message = "Panel de Usuario.";
+
+            return View();
+        }
+    }
+}
