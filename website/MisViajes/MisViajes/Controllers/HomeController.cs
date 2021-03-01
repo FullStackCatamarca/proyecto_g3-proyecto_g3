ddbbc58cfb41e5f2fd147dcaf4f180b81d8666bf
JhonyDV <jony.cas998@gmail.com>
2021-03-01 10:38:22 -0300

    Agregamos una barra Para consultar el clima de las localidades.

--- website/MisViajes/MisViajes/Controllers/HomeController.cs
+++ website/MisViajes/MisViajes/Controllers/HomeController.cs
@@ -3,14 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MisViajes.Models;
 
 namespace MisViajes.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
+
         {
+            
             return View();
+        }
+
+        public JsonResult ObtenerDepartamentos()
+        {
+            List<Departamentos> lst;
+            using (misviajesEntities db = new misviajesEntities())
+            {
+
+                lst = (from d in db.Departamentos
+                       select d).ToList();
+            }
+            return Json(lst,JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+
+        public JsonResult ObtenerLocalidades(int? idDepartamento)
+        {
+            List<Localidades> lst;
+            using (misviajesEntities db = new misviajesEntities())
+            {
+
+                lst = (from d in db.Localidades
+                       where d.Departamento == idDepartamento
+                       select d).ToList();
+            }
+            return Json(lst, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult About()
