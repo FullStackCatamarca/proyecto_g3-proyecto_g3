f581ecea6712f44b3bd7d88d1b1943aa54588bd1
cesarweb <cesarweb@hotmail.com>
2021-03-21 01:33:15 -0300

    Añadido algunos cambios agregados bundle y filter

--- website/MisViajes/MisViajes/App_Start/RouteConfig.cs
+++ website/MisViajes/MisViajes/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MisViajes
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Foro", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
